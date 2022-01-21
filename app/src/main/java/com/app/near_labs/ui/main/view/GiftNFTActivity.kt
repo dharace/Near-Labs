cde1c1ffc0b1b91acae6c2c7b06047f60fdee44b
Dell <dhara.ce01@gmail.com>
2022-01-21 08:54:52 -0500

    First Commit

--- app/src/main/java/com/app/near_labs/ui/main/view/GiftNFTActivity.kt
+++ app/src/main/java/com/app/near_labs/ui/main/view/GiftNFTActivity.kt
@@ -13,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.near_labs.R
-import com.app.near_labs.data.model.NFTUser
 import com.app.near_labs.data.model.User
 import com.app.near_labs.ui.main.adapter.GiftNFTAdapter
 import com.app.near_labs.ui.main.viewmodel.UserViewModel
@@ -22,7 +21,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_gift_n_f_t.*
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.dilog_allow_contacts.view.*
-import java.io.Serializable
 import javax.inject.Inject
 
 
@@ -169,17 +167,4 @@ class GiftNFTActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    // dummy list of users
-    fun getDummyUsers(): List<NFTUser> {
-        return listOf(
-            NFTUser("Darlene Robertson", "@johndoe", null, false),
-            NFTUser("Jacob Jones", "@johndoe", resources.getDrawable(R.drawable.avtar1), false),
-            NFTUser("Jenny Wilson", "@johndoe", null, true),
-            NFTUser("Ronald Richards", "@johndoe", null, true),
-            NFTUser("Cameron Williamson", "@johndoe", null, false),
-            NFTUser("Courtney Henry", "@johndoe", null, false),
-            NFTUser("Wade Warren", "@johndoe", null, false),
-            NFTUser("Darrell Steward", "@johndoe", resources.getDrawable(R.drawable.avtar1), true),
-        )
-    }
 }
